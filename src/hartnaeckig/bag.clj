d1c28808b7afe8125721d004cfb89e5c9d69e53f
FiV0 <finn.volkel@gmail.com>
2022-05-29 23:33:47 +0200

    bag as clojure core protocols extension

--- src/hartnaeckig/bag.clj
+++ src/hartnaeckig/bag.clj
@@ -33,3 +33,38 @@
   (count' b "foo")
   (as-set b)
   (size b))
+
+(deftype Bag [size content]
+  clojure.lang.IPersistentSet
+
+  (seq [this] (seq content))
+
+  (count [this] size)
+
+  (cons [this o]
+    (Bag. (inc size) (update content o (fnil + 0) 1)))
+
+  (empty [this] (Bag. 0 {}))
+
+  (equiv [this other] (= this other))
+
+  (disjoin [this key]
+    (let [cnt (get content key)]
+      (Bag. (dec size)
+            (cond
+              (nil? cnt) (throw (ex-info "No such key" {:key key}))
+              (= cnt 1) (dissoc content key)
+              :else (update content key - 1)))))
+
+  (contains [this key] (some? (get content key)))
+
+  (get [this key] (get content key)))
+
+(defn bag [] (Bag. 0 {}))
+
+(comment
+  (def b (-> (bag) (conj "foo") (conj "foo") #_(disj "foo") (conj "bar")))
+  (count b)
+  (get b "foo")
+  (contains? b "foo")
+  (seq b))
